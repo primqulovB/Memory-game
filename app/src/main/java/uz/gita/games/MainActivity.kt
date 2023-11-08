8b0505ee60e327610c0e1e799097269835e1e227
bahrom <bahromprimqulov2003@gmail.com>
2023-11-08 15:51:17 +0500

    Commit 2023.08.11 45 files added

---
+++ app/src/main/java/uz/gita/games/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.gita.games
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
